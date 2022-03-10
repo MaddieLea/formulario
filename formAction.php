bd32fff5769e28ea1709e6da1e6b0bf5c09d97b0
MaddieLea <miguleonardo2@gmail.com>
2022-03-10 17:16:51 -0300

    Adicionando um form novo e o php com action

---
+++ formAction.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>O primeiro código em PHP</title>
+</head>
+<body>
+    <h1>
+ <?php 
+echo "Hello World, and hello Adriana.";
+$num = 1; 
+
+if($num === 1){
+    echo "Hello World!!!";
+}
+else if($num == 2) {
+     echo "Hello Adriana!!!";
+ }
+?>
+ </h1>
+</body>
+</html>
